bc58d50910e6611338d5b9ca4fa32851982c8d86
radeeshm <radeeshm@gmail.com>
2021-03-22 11:12:20 +1100

    chnages for Monday

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+using Shopping.API.Data;
+using Shopping.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly ProductContext _context;
+
+        public ProductController(ILogger<ProductController> logger, ProductContext context)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            return await _context.Products.Find(p => true).ToListAsync();
+        }
+    }
+}
